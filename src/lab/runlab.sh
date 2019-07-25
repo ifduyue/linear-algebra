13e4ce7292c7793458b4fbcc25c2a26860ef5258
Jim Hefferon <jhefferon@smcvt.edu>
2019-07-25 17:26:32 -0400

    tentatively add lab files using pythontex

---
+++ src/lab/runlab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Generate a copy of the lab document.
+# 2019-July-25 Jim Hefferon GPL 3
+TEXINPUTS="/home/ftpmaint/Documents/linear-algebra.git/src//:$TEXINPUTS" xelatex lab
+python3 /usr/local/share/texmf/tex/latex/pythontex/pythontex.py lab
+TEXINPUTS="/home/ftpmaint/Documents/linear-algebra.git/src//:$TEXINPUTS" xelatex lab
