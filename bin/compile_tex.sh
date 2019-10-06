abeda9b084e1f59416afb0befa2b95038f0f4556
Jim Hefferon <jhefferon@smcvt.edu>
2019-10-06 13:48:00 -0400

    reported bugs almost all addressed

--- bin/compile_tex.sh
+++ bin/compile_tex.sh
@@ -3,9 +3,7 @@
 # TeX search directory variables.
 # 2019-Oct-05 Jim Hefferon GPL 2.
 
-echo "Apply LaTeX to:" "$@"
 SRCDIR=`expr match "$PWD" '\(.*/linear-algebra.git/src\)'`
-echo "SRCDIR is ${SRCDIR}"
-
-echo "TEXINPUTS is ${TEXINPUTS}"
+# echo "SRCDIR is ${SRCDIR}"
+# echo "TEXINPUTS is ${TEXINPUTS}"
 TEXINPUTS="${SRCDIR}//:$TEXINPUTS" pdflatex "$@"
