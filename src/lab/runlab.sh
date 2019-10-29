25450a3116efd9ec2d4f89d6b52c5b5882857317
Jim Hefferon <jhefferon@smcvt.edu>
2019-10-29 09:22:07 -0400

    fonts for lab manual

--- src/lab/runlab.sh
+++ src/lab/runlab.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Generate a copy of the lab document.
 # 2019-July-25 Jim Hefferon GPL 3
-TEXINPUTS="/home/ftpmaint/Documents/linear-algebra.git/src//:$TEXINPUTS" xelatex lab
-python3 /usr/local/share/texmf/tex/latex/pythontex/pythontex.py lab
-sage lab.sagetex.sage
-bibtex lab
-TEXINPUTS="/home/ftpmaint/Documents/linear-algebra.git/src//:$TEXINPUTS" xelatex lab
+FN="lab"
+TEXINPUTS="/usr/local/src/sage/local/share/texmf/tex/latex/sagetex//:/home/ftpmaint/Documents/linear-algebra/src/sty//:$TEXINPUTS"
+
+TEXINPUTS=${TEXINPUTS} xelatex "./${FN}"
+python3 ${PYTHONTEX_DIR}/pythontex.py "${FN}"
+sage ${FN}.sagetex.sage
+# # bibtex lab
+TEXINPUTS=${TEXINPUTS} xelatex "./${FN}"
