641dbcc6248398efdc2ef2e34b5ca5152d734d92
Jim Hefferon <jhefferon@smcvt.edu>
2019-11-01 10:50:08 -0400

    fix python call for python tex in runlab.sh

--- src/lab/runlab.sh
+++ src/lab/runlab.sh
@@ -2,7 +2,9 @@
 # Generate a copy of the lab document.
 # 2019-July-25 Jim Hefferon GPL 3
 FN="lab"
+
 TEXINPUTS="/usr/local/src/sage/local/share/texmf/tex/latex/sagetex//:/home/ftpmaint/Documents/linear-algebra/src/sty//:$TEXINPUTS"
+PYTHONTEX_DIR="/usr/local/share/texmf/tex/latex/pythontex"
 
 TEXINPUTS=${TEXINPUTS} xelatex "./${FN}"
 python3 ${PYTHONTEX_DIR}/pythontex.py "${FN}"
